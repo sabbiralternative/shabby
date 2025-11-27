2d8fab345b448029ebcd97ee293a9a7ddc43329e
sabbirHossain <sabbirshnt@gmail.com>
2025-11-27 11:58:33 +0600

    done

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { settings } from "../../utils";
+import { useIndex } from "../../hooks";
 
 const NotFound = () => {
+  const { mutate } = useIndex();
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -17,6 +19,7 @@ const NotFound = () => {
         const splitPath = location.pathname.split("/");
         const lastDigit = splitPath[splitPath?.length - 1];
         if (parseFloat(lastDigit)) {
+          mutate({ type: "addReferralCount", referral_id: lastDigit });
           localStorage.setItem("referralCode", lastDigit);
           navigate("/register");
         }
