918d2432b7e5f65ad432b00aa53c9ac293f6f355
sabbirHossain <sabbirshnt@gmail.com>
2025-12-02 11:29:45 +0600

    done

--- src/pages/Affiliate/Affiliate.jsx
+++ src/pages/Affiliate/Affiliate.jsx
@@ -1,19 +1,33 @@
+import { Fragment, useState } from "react";
 import BonusInformation from "../../components/modules/Affiliate/BonusInformation";
 import InviteSection from "../../components/modules/Affiliate/InviteSection";
 import TodayProfitLoss from "../../components/modules/Affiliate/TodayProfitLoss";
 import TodayStatusSection from "../../components/modules/Affiliate/TodayStatusSection";
 import TopFiveLossUser from "../../components/modules/Affiliate/TopFiveLossUser";
 import "./affiliate.css";
+import UserList from "../../components/modules/Affiliate/UserList";
+import ProfitLoss from "../../components/modules/Affiliate/ProfitLoss";
+import Reports from "../../components/modules/Affiliate/Reports";
+import Footer from "../../components/modules/Affiliate/Footer";
 
 const Affiliate = () => {
+  const [tab, setTab] = useState("dashboard");
   return (
     <div className="center-container" style={{ width: "100%" }}>
       <div data-v-4c49d924 className="container">
-        <TodayStatusSection />
-        <InviteSection />
-        <TopFiveLossUser />
-        <BonusInformation />
-        <TodayProfitLoss />
+        {tab === "dashboard" && (
+          <Fragment>
+            <TodayStatusSection />
+            <InviteSection />
+            <TopFiveLossUser />
+            <BonusInformation />
+            <TodayProfitLoss />
+          </Fragment>
+        )}
+        {tab === "user-list" && <UserList />}
+        {tab === "pnl" && <ProfitLoss />}
+        {tab === "reports" && <Reports />}
+        <Footer setTab={setTab} tab={tab} />
       </div>
     </div>
   );
