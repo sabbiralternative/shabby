72b45affd8c2633a20c71dd9528ec65f8c5c80a4
sabbirHossain <sabbirshnt@gmail.com>
2026-01-10 18:51:05 +0600

    done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -390,7 +390,7 @@ const Header = () => {
     const newVersion = socialLink?.build_version;
     if (!stored_build_version) {
       if (newVersion) {
-        setShowBuildVersion(true);
+        localStorage.setItem("build_version", newVersion);
       }
     }
     if (stored_build_version && newVersion) {
